9b7e4f7a3ec98598011cc62cda5eb83a11947356
wangkai998 <1317851618@qq.com>
2020-08-04 00:11:16 +0800

    day01作业

---
+++ day01_ssm/src/main/java/com/xiaoshu/dao/DevicetypeMapper.java
@@ -0,0 +1,24 @@
+package com.xiaoshu.dao;
+
+import java.util.List;
+
+import com.xiaoshu.entity.Devicetype;
+import com.xiaoshu.entity.User;
+
+public interface DevicetypeMapper {
+
+	List<Devicetype> findUserPage(Devicetype devicetype);
+
+
+
+	void updateDevice(Devicetype devicetype);
+
+	void addDevice(Devicetype devicetype);
+
+	void deleteDevice(int parseInt);
+
+
+
+	Devicetype existdeviceWithdeviceName(String devicename);
+
+}
